712b44056da414cd6bb465d9693f6c4bd9990f3e
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-16 20:17:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,13 @@ def turn_count(board)
   counter = 0 
   if counter % 2 
   counter += 1 
-  
 end
-  
+
+def current_player
+  if turn_count(board).even 
+    puts X 
+  else 
+    puts O 
+  end
+end 
   
