988ccd25f1af01b8b898891b3f9306348b4d4b0a
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-16 20:40:35 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0 
   board.each do |value|
-    
+    if value == "X" || value == "O"
     counter += 1 
   end
 end
