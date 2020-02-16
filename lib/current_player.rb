6454c3dc440133e458bf539ea4c8f9e81eff8c06
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-16 20:22:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,15 @@
 def turn_count(board)
   counter = 0 
-  if valid 
+  if puts X or O 
   counter += 1 
+end
 end
 
 def current_player
   if turn_count(board).even?
-    puts X 
-  else 
     puts O 
+  else 
+    puts X
   end
 end 
   
