3f2d138d4936a33ffb0c977bd508afdb10aeed24
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-16 20:29:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   counter = 0 
-  if (board[index] == "X" || board[index] == "O") counter += 1 
+  if (board[index] == "X" || board[index] == "O") 
+    counter += 1 
   end
 end
 
